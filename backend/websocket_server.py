4cb1592a94ffc6498aa3a409e700d967acbf50fb
csierr <catalina.sierra@uc.cl>
2025-11-17 18:08:08 -0300

    chore: refactor WebSocket client for OpenAI Realtime API and add utility functions

--- backend/websocket_server.py
+++ backend/websocket_server.py
@@ -1,22 +1,25 @@
-from fastapi import FastAPI, WebSocket
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
 import os
 import asyncio
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
 from .my_realtime_client import MyRealtimeClient
+from .utils import load_prompt
 from dotenv import load_dotenv
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+LANGUAGE = os.getenv("LANGUAGE")
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="public", html=True), name="static")
 
-
-@app.get("/")
-async def get_index():
-    return FileResponse("public/index.html")
-
+# CORS middleware to allow requests from the frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # FE's origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -25,13 +28,13 @@ async def websocket_endpoint(websocket: WebSocket):
     client = MyRealtimeClient(
         websocket=websocket,
         api_key=OPENAI_API_KEY,
-        instructions="You're kind, friendly and helpful. Please start speaking in English, and then answer accordingly in the language the user talks to you.",
+        instructions=load_prompt("instructions.txt").format(l1=LANGUAGE, l2=LANGUAGE),
         voice="sage"
     )
 
     async def receive_from_frontend():
         async for data in websocket.iter_json():
-            print("📨 Texto recibido del navegador:", data)
+            print("📨 Text received from browser:", data)
             if data.get("type") == "text":
                 await client.send_text(text=data["data"])
 
@@ -41,6 +44,6 @@ async def websocket_endpoint(websocket: WebSocket):
             receive_from_frontend()  # Listens to the browser and forwards to OpenAI
         )
     except Exception as e:
-        print("Error websocket_server.py:", e)
+        print("Error in websocket_server.py:", e)
     finally:
         await client.clean_up()
